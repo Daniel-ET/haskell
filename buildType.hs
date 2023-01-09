6b4b20ddd5bd9ff8f2474d4cb24f44bfb540533a
Dan Ettah <96924468+Daniel-ET@users.noreply.github.com>
2023-01-09 20:29:05 +0000

    Add files via upload

---
+++ buildType.hs
@@ -0,0 +1,14 @@
+--This is a type definition specifying the input and output type of tokenize
+--tokenize :: String -> String
+
+--Constructor for an Animal that can be a Dog or a Cat.
+data Animal = Dog | Cat
+
+--getAnimalSound type
+getAnimalSound :: Animal -> String
+
+getAnimalSound Dog = "woof"
+
+getAnimalSound Cat = "meow"
+
+main = print $ getAnimalSound Dog
